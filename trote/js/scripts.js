dd67d014a151dfd00601b6b2befe76389ec9e8c4
Guilherme Tonin <gtoninjorge@gmail.com>
2025-04-10 01:08:23 -0300

    Finalização das seções e JS

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,26 +1,104 @@
 function calcular() {
-  // let declaração variavel sem tipo //
-  let soma = 0;
+  // declaração variável sem tipo
+  let soma = 0, acaoSocial, homenagem, leite, kit, equipe, suplemento, pontosKitSupl = 0, sangue, pontosSangue = 0
 
-  // recupera um valor da acaoSocial digitado //
-  let acaoSocial = document.getElementById("acaoSocial").value;
-  soma = soma + Number(acaoSocial);
-  // recupera o valor da homenagem digitado //
-  let homenagem = document.getElementById("homenagem").value;
-  soma = soma + Number(homenagem);
-  // recupera a quantidade de litros de leite //
-  let leite = document.getElementById("leite").value;
-  soma = soma + Number(2 * Number(leite));
-  // recupera o kit alimentacao //
-  let kitAvulso = document.getElementById("kitAvulso").value;
-  soma = soma + Number(30 * Number(kitAvulso));
-  // recupera o suplemento //
-  let suplementoAvulso = document.getElementById("suplementoAvulso").value;
-  soma = soma + Number(15 * Number(suplementoAvulso));
-  let arrozCinco = document.getElementById("arrozCinco").value;
-  soma = soma + Number(5 * Number(arrozCinco));
-  // devolve o resultado para o HTML //
-  document.getElementById("soma").innerHTML = soma.toFixed(2);
-  // to.Fixed(2) = formatar números com casas decimais //
-  // innerHTML = coloca a soma dentro do HTML //
+  // recupera o valor da ação social digitado
+  acaoSocial = Number(document.getElementById("acaoSocial").value)
+  soma = soma + acaoSocial
+
+  // recupera o valor da homenagem digitado
+  homenagem = Number(document.getElementById("homenagem").value)
+  soma = soma + homenagem
+
+  // recupera a qtde de litros de leite
+  leite = Number(document.getElementById("leite").value)
+  soma = soma + (2 * leite)
+
+  // recupera a qtde de kits 
+  kit = Number(document.getElementById("kit").value)
+
+  kit = Number(document.getElementById(""))
+
+
+
+  // recupera a cor da equipe
+  equipe = document.getElementById("equipe").value
+
+  // recupera a qtde de latas de suplemento
+  suplemento = document.getElementById("suplemento").value
+
+  if (equipe == "Laranja") {
+    // equipe é laranja
+    if (kit >= 97 && suplemento >= 49) {
+      pontosKitSupl = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+    }
+    else if (kit >= 78 && suplemento >= 39) {
+      pontosKitSupl = 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+    }
+    else if (kit >= 49 && suplemento >= 25) {
+      pontosKitSupl = 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+    }
+    else if (kit >= 19 && suplemento >= 10) {
+      pontosKitSupl = 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+    }
+  }
+  else if (equipe == "Preta") {
+
+  }
+  else if (equipe == "Roxa") {
+
+  }
+  else if (equipe == "Verde") {
+
+  }
+  else { // assumo que a equipe é vermelha
+
+  }
+  soma = soma + pontosKitSupl
+
+  // sangue
+  sangue = Number(document.getElementById("sangue").value)
+
+  if (equipe == "Laranja") {
+    if (sangue >= 49) {
+      pontosSangue = 2500 + (sangue - 49) * 20
+    }
+    else {
+      pontosSangue = sangue * 20
+    }
+  }
+  else if (equipe == "Preta") {
+    if (sangue >= 52) {
+      pontosSangue = 2500 + (sangue - 52) * 20
+    }
+    else {
+      pontosSangue = sangue * 20
+    }
+  }
+  else if (equipe == "Roxa") {
+    if (sangue >= 51) {
+      pontosSangue = 2500 + (sangue - 51) * 20
+    }
+    else {
+      pontosSangue = sangue * 20
+    }
+  }
+  else if (equipe == "Verde") {
+    if (sangue >= 44) {
+      pontosSangue = 2500 + (sangue - 44) * 200
+    }
+    else {
+      pontosSangue = sangue * 20
+    }
+  }
+  else {
+    if (sangue >= 47) {
+      pontosSangue = 2500 + (sangue - 47) * 200
+    }
+  }
+  // soma o sangue
+  soma = soma + pontosSangue
+
+  // devolve o resultado para o HTML
+  document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
