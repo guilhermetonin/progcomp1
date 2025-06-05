23b49d2a3c53a0320b43486e82279ce79a86332a
Guilherme Tonin <gtoninjorge@gmail.com>
2025-06-04 22:11:18 -0300

    Exercícios - Estrutura de Repetição

--- estrutura-repeticao/vetor/js/exemplo.js
+++ estrutura-repeticao/vetor/js/exemplo.js
@@ -21,3 +21,86 @@ function exe1() {
   console.log(`Quantidade de Impares: ${qtdeImpares}`)
 
 }
+
+
+// ler um vetor de 10 elementos inteiros
+// some 10 nos elementos da posicoes pares
+// multiplique por 5 nos elementos das posicoes impares
+
+// exercicio 1 - prova
+
+function exe2() {
+
+  let vet = [] // declaro vetor
+
+  for (let i = 0; i < 2; i++) {
+    vet.push(Number(prompt(` Informe o ${i + 1}o. número `)))
+  }
+
+  for (let i = 0; i < 2; i++) {
+    if (i % 2 == 0) { // posicao par
+      vet[i] = vet[i] + 10
+    }
+    else { // posicao impar
+      vet[i] = vet[i] * 5
+    }
+  }
+  console.log(`${vet}`)
+
+}
+
+// exercicio 2 - prova
+
+function exe3() {
+
+  let vet = []
+  let nota1 = 0, nota2 = 0, nota3 = 0
+
+  for (let i = 0; i < 10; i++) {
+    do {
+      vet[i] = Number(prompt(`Informe a nota: 1- Gostou Muito 2- Gostou 3- Não gostou`))
+    } while (vet[i] > 3 || vet[i] < 1) //  vet menor que 3 ou vet maior que 1
+  }
+
+  for (let i = 0; i < 10; i++) {
+    if (vet[i] == 1) {
+      nota1++
+    }
+    else if (vet[i] == 2) {
+      nota2++
+    }
+    else { // assumo que é nota 3
+      nota3++
+    }
+  }
+
+  console.log(`Nota 1: ${nota1} pessoas | Nota 2: ${nota2} pessoas | Nota 3: ${nota3} pessoas`)
+  console.log(`Nota 3: ${(nota3 / vet.length) * 100}%`)
+
+}
+
+
+
+// 
+function exe4() {
+
+  let vet = [], aux[]
+  let media = 0
+
+  for (let i = 0; i < 8; i++) {
+    vet[i] = Number(prompt(`${i}o. número: `))
+  }
+  for (let i = 0; i < 8; i++) {
+    media += vet[i]
+  }
+  media = media / vet.length
+
+}
+
+function exe5() {
+}
+
+function exe6() {
+}
+
+
